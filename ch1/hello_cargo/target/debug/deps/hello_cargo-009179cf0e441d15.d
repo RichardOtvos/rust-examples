a02ebbf0af58c72d86059a24aa37e6dc618911b7
Richard Otvos <richard.otvos@cloudera.com>
2019-07-24 20:22:03 +0200

    Ch1 of the Rust book

---
+++ ch1/hello_cargo/target/debug/deps/hello_cargo-009179cf0e441d15.d
@@ -0,0 +1,5 @@
+/Users/richardotvos/Code/rust-examples/ch1/hello_cargo/target/debug/deps/hello_cargo-009179cf0e441d15: src/main.rs
+
+/Users/richardotvos/Code/rust-examples/ch1/hello_cargo/target/debug/deps/hello_cargo-009179cf0e441d15.d: src/main.rs
+
+src/main.rs:
---
+++ ch1/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/richardotvos/Code/rust-examples/ch1/hello_cargo/target/debug/hello_cargo.dSYM: /Users/richardotvos/Code/rust-examples/ch1/hello_cargo/src/main.rs
